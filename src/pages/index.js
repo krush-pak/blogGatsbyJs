dcd33f283eae79722981f35a39eff4a68721044f
Jonas Jerusalem <jonas.jerusalem@gmail.com>
2019-10-07 06:53:22 +0200

    added Banner and Hero Components to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
+
 export default () => (
-  <>
-    <Layout>
-      Hello world!
-      <Link to="/blog">blog page</Link>
-    </Layout>
-  </>
+  <Layout>
+    <SimpleHero>
+      <Banner
+        title="continue exploring"
+        info="dawdnw awdhawidhwai a ha ih aih iahai aia aipdadwoad"
+      >
+        <Link to="/tours" className="btn-white">
+          Explore Tours
+        </Link>
+      </Banner>
+    </SimpleHero>
+  </Layout>
 )
