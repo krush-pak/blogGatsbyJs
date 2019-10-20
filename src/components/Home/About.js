07fdc15f39c0e7c39febc840246b6eee1805d93b
Jonas Jerusalem <Jonny@Macs-MacBook-Pro.local>
2019-10-20 15:45:17 +0200

    added About and Services Section

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,12 +1,33 @@
 import React from "react"
 import Title from "../Title"
+import styles from "../../css/about.module.css"
+import img from "../../images/defaultBcg.jpeg"
 
 const About = () => {
   return (
-    <div>
-      hello from about
+    <section className={styles.about}>
       <Title title="about" subtitle="us" />
-    </div>
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <div className={styles.imgContainer}>
+            <img src={img} alt="about company" />
+          </div>
+        </article>
+        <article className="aboutInfo">
+          <h4>explore the difference</h4>
+          <p>
+            lorem dawdowdjwodjaowdmawod adawodkawokdowa kdoakw doawk doakw dokaw
+          </p>
+          <p>
+            dawodkaowda awo jdawodk aow koa a kwao kodak
+            okawodkawodkawodkawodkaowkdodwak
+          </p>
+          <button type="button" className="btn-primary">
+            read more
+          </button>
+        </article>
+      </div>
+    </section>
   )
 }
 
---
+++ src/constants/services.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { FaWallet, FaTree, FaSocks } from "react-icons/fa"
+
+export default [
+  {
+    icon: <FaWallet />,
+    title: "saving money",
+    text: "dawidjwiadjiwajdawdijawdiadjwidwjadawd",
+  },
+  {
+    icon: <FaTree />,
+    title: "endless hiking",
+    text: "dawidjwiadjiwajdawdijawdiadjwidwjadawd",
+  },
+  {
+    icon: <FaSocks />,
+    title: "amazing comfort",
+    text: "dawidjwiadjiwajdawdijawdiadjwidwjadawd",
+  },
+]
